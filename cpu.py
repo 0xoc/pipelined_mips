8528ca3342ab58f51349e1dd915af5a93df9b1a0
Sina Parvizi <spsina@Sinas-MacBook-Air.local>
2020-01-18 18:50:09 +0330

    basic fetch

--- cpu.py
+++ cpu.py
@@ -3,11 +3,17 @@ from memory import Memory
 from alu import ALU
 from register_file import RegisterFile
 
+FETCH = '0'
+DECODE = '1'
+EXC = '2'
+MEM = '3'
+WB = '4'
 
 class ControlUnit:
-
+    pass
 
 class CPU:
+
     def __init__(self):
         self._instruction_memory = Memory(1024, byte_size=BYTE_SIZE)
         self._data_memory = Memory(1024, byte_size=BYTE_SIZE)
@@ -15,10 +21,78 @@ class CPU:
         self._alu = ALU()
         self._pc = ALU.int_to_n_bit_binary(0)
 
-    def fetch(self):
-        n = WORD // BYTE_SIZE
+    def load_instructions(self, instructions):
+        memory_cell_size = self._instruction_memory.byte_size
+        n = WORD // memory_cell_size
 
-        # load n cells from instruction memory
-        instruction = []
+        base_addr = ALU.int_to_n_bit_binary(0)
+        self._alu.set_input_1(base_addr)
+        
+        self._instruction_memory.set_mem_write(True)
+
+        for instruction in instructions:
+            inst = [int(c) for c in instruction.replace('\n','')]
+
+            for i in range(n):
+                
+                # caculate write addr
+                self._alu.set_input_2(ALU.int_to_n_bit_binary(i))
+                self._alu.set_op('00')
+                addr = self._alu.result
+
+                # write data
+                unit_data = tuple(inst[i*memory_cell_size: (i+1) * memory_cell_size])
+
+                self._instruction_memory.set_write_addr(addr)
+                self._instruction_memory.set_write_data(unit_data)
+
+            self._alu.set_input_2(ALU.int_to_n_bit_binary(4))
+            self._alu.set_input_1(self._alu.result)
+
+
+    def _load_w(self, mem, base_addr):
+        n = WORD // mem.byte_size
+        
+        self._alu.set_input_1(base_addr)
+        self._alu.set_op('00')
+        
+        word = []
+        
         for i in range(n):
+            self._alu.set_input_2(ALU.int_to_n_bit_binary(i))
+
+            addr = self._alu.result
+
+            self._instruction_memory.set_read_address(addr)
+            word += list(self._instruction_memory.read_result)
+        
+        return word
+
+    def set_control_signals(self, stage):
+        if stage == FETCH:
+            self._instruction_memory.set_mem_read(True)
+
+        elif stage == DECODE:
             pass
+        elif stage == EXC:
+            pass
+        elif stage == MEM:
+            pass
+        elif stage == WB:
+            pass
+        else:
+            raise Exception("Invalid stage")
+
+    def fetch(self):
+        self.set_control_signals(FETCH)
+
+        instruction = self._load_w(self._instruction_memory, self._pc)
+
+        self._alu.set_input_1(self._pc)
+        self._alu.set_input_2(ALU.int_to_n_bit_binary(4))
+        self._alu.set_op('00')
+
+        self._pc = self._alu.result
+        
+        return instruction
+        
--- main.py
+++ main.py
@@ -1,1 +1,16 @@
 import tests
+
+from alu import ALU
+from cpu import CPU
+
+program = open("program.inst")
+instructions = program.readlines()
+
+cpu = CPU()
+
+cpu.load_instructions(instructions)
+
+cpu._instruction_memory.set_mem_read(True)
+
+print(cpu.fetch())
+print(cpu.fetch())
